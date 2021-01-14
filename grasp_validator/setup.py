b41640fc0b49d0f2ac313c06dde8faaad3de8b77
Tan Jing Peng Glenn <glenn_tan@artc.a-star.edu.sg>
2021-01-14 18:28:59 +0800

    Re-Added python package marker generated by ros2cli to remove warnings
    Signed-off-by: Tan Jing Peng Glenn <glenn_tan@artc.a-star.edu.sg>

--- grasp_validator/setup.py
+++ grasp_validator/setup.py
@@ -7,6 +7,8 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
