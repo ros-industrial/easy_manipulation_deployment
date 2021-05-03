e98ea77e9ee11158bcc74f625483e9561f4affff
Glenn Tan <glenn_tan@artc.a-star.edu.sg>
2021-05-03 10:17:46 +0800

    Added Tests for finger collision (Now works with CMakelists Hack)

--- easy_manipulation_deployment/grasp_planner/test/multifinger_test.cpp
+++ easy_manipulation_deployment/grasp_planner/test/multifinger_test.cpp
@@ -1460,41 +1460,43 @@ TEST_F(MultiFingerTest, getOpenFingerCoordinatesTest)
   
 }
 
-//ERRORS WITH EIGEN + FCL. CHeck again
+// Test works with compiler hackish solution
 TEST_F(MultiFingerTest, checkFingerCollisionTest)
 {
-  // GenerateObjectVertical();
-  // ResetVariables();
-  // gripper_stroke = 0.04;
-  // ASSERT_NO_THROW(LoadGripper());
+  GenerateObjectVertical();
+  ResetVariables();
+  gripper_stroke = 0.04;
+  ASSERT_NO_THROW(LoadGripper());
 
   
-  // grasp_planner::collision::Box * collision_object_shape =
-  //   new grasp_planner::collision::Box(0.01, 0.05, 0.02);
+  grasp_planner::collision::Box * collision_object_shape =
+    new grasp_planner::collision::Box(0.01, 0.05, 0.02);
 
-  // grasp_planner::collision::Transform finger_transform;
-  
-  // // fcl::Transform3<float> collision_object_transform = fcl::Transform3<float>::Identity();
-  // grasp_planner::collision::Transform collision_object_transform;
-  // collision_object_transform.setIdentity();
+  grasp_planner::collision::Transform finger_transform;
 
-  // #if FCL_VERSION_0_6_OR_HIGHER == 1
-  //   std::cout << " Version 0.6 or higher is 1" << std::endl;
-  //   collision_object_transform.translation() << 0, 0, 0;
-  // #else
-  //   std::cout << " Version 0.6 or higher is zero" << std::endl;
-  //   collision_object_transform.setTranslation(
-  //     grasp_planner::collision::Vector(0, 0, 0));
-  // #endif
+  // fcl::Transform3<float> collision_object_transform = fcl::Transform3<float>::Identity();
+  grasp_planner::collision::Transform collision_object_transform;
+  collision_object_transform.setIdentity();
 
-  // grasp_planner::collision::CollisionObject collision_object(
-  //   std::shared_ptr<grasp_planner::collision::CollisionGeometry>(collision_object_shape), fcl::Transform3<float>::Identity());
+  #if FCL_VERSION_0_6_OR_HIGHER == 1
+    std::cout << " Version 0.6 or higher is 1" << std::endl;
+    collision_object_transform.translation() << 0, 0, 0;
+  #else
+    std::cout << " Version 0.6 or higher is zero" << std::endl;
+    collision_object_transform.setTranslation(
+      grasp_planner::collision::Vector(0, 0, 0));
+  #endif
 
-  // std::shared_ptr<grasp_planner::collision::CollisionObject> collision_object_ptr =
-  //   std::make_shared<grasp_planner::collision::CollisionObject>(collision_object);
+  grasp_planner::collision::CollisionObject collision_object(
+    std::shared_ptr<grasp_planner::collision::CollisionGeometry>(collision_object_shape), fcl::Transform3<float>::Identity());
 
-  // Eigen::Vector3f finger_point_sample_center(0.005, 0.025, 0.01);
-  // EXPECT_TRUE(gripper->checkFingerCollision(finger_point_sample_center, collision_object_ptr));
+  std::shared_ptr<grasp_planner::collision::CollisionObject> collision_object_ptr =
+    std::make_shared<grasp_planner::collision::CollisionObject>(collision_object);
+
+  Eigen::Vector3f finger_point_sample_center(0.005, 0.025, 0.01);
+  Eigen::Vector3f finger_point_far_away(0.1, 0.25, 0.1);
+  EXPECT_TRUE(gripper->checkFingerCollision(finger_point_sample_center, collision_object_ptr));
+  EXPECT_FALSE(gripper->checkFingerCollision(finger_point_far_away, collision_object_ptr));
 }
 
 TEST_F(MultiFingerTest, getNearestPlaneIndexTest)
